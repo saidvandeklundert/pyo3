42d80bea99dd22b707cf2e0b0ba2cb33998f9f9f
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-15 19:30:28 +0100

    adding things

--- pyo3/example.py
+++ pyo3/example.py
@@ -90,12 +90,14 @@ for i in range(10):
 
 py_start = timer()
 for i in range(999):
-    get_fibonacci(150)
+    res = get_fibonacci(150)
+    print(res)
 py_res = get_fibonacci(150)
 py_elapsed = round(timer() - py_start, 3)
 ru_start = timer()
 for i in range(999):
-    rust.get_fibonacci(150)
+    res = rust.get_fibonacci(150)
+    print(res)
 ru_res = rust.get_fibonacci(150)
 ru_elapsed = round(timer() - ru_start, 5)
 print("Calculating the 150th fibonacci number 1000 times.")

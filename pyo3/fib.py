febd3ee7d4ded122930162853318ecd24b77216f
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-16 14:55:59 +0100

    adding

--- pyo3/fib.py
+++ pyo3/fib.py
@@ -8,7 +8,7 @@ def get_fibonacci(number: int) -> int:
     total = 0
     last = 0
     current = 1
-    for _ in range(1, number - 1):
+    for _ in range(1, number):
         total = last + current
         last = current
         current = total

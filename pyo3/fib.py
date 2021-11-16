0112e7dc8e324b4f88153343e1328889fa1b8fea
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-16 20:16:52 +0100

    adding

--- pyo3/fib.py
+++ pyo3/fib.py
@@ -1,5 +1,5 @@
 def get_fibonacci(number: int) -> int:
-    """Get the nth fibonacci number."""
+    """Get the nth Fibonacci number."""
     if number == 1:
         return 1
     elif number == 2:
@@ -13,18 +13,3 @@ def get_fibonacci(number: int) -> int:
         last = current
         current = total
     return total
-
-
-def get_fibonacci_alt(n: int) -> int:
-    """Get the nth fibonacci number."""
-    fib_seq = [0]
-    while n > 0:
-        n -= 1
-        if len(fib_seq) == 1:
-            fib_seq.append(1)
-        else:
-            last = fib_seq[-1]
-            second_to_last = fib_seq[-2]
-            fib_seq.append(last + second_to_last)
-
-    return fib_seq[-1]

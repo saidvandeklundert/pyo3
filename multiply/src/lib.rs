241fa392b587d1bb3f557c2b07c16369a434f0a0
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-16 20:17:56 +0100

    adding

--- multiply/src/lib.rs
+++ multiply/src/lib.rs
@@ -6,7 +6,7 @@ fn multiply(a: isize, b: isize) -> PyResult<isize> {
 }
 
 #[pymodule]
-fn rust(_py: Python, m: &PyModule) -> PyResult<()> {
+fn rusty(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(multiply, m)?)?;
     Ok(())
 }

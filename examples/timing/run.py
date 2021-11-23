b7f68c0c8203d1091ca16269b2989fb21f5f6dc3
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-23 20:44:50 +0100

    adding

--- examples/timing/run.py
+++ examples/timing/run.py
@@ -29,8 +29,8 @@ calling_python_function = round(timer() - start, 5)
 print(
     f"""Made {function_calls} to multiply in Rust as well as in Python.
 
-Time spend calling the Rust function: {calling_rust_function}
+\t{function_calls} to the Rust 'timing_example.multiply' function:\t\t{calling_rust_function} 
 
-Time spend calling the Python function: {calling_python_function}
+\t{function_calls} to the Python 'multiply' function:\t\t{calling_python_function}
 """
 )

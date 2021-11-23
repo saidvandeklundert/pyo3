31dba442a087cc28c60842573f3bff0aa5c9347b
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-23 20:29:37 +0100

    adding

--- examples/timing/run.py
+++ examples/timing/run.py
@@ -3,8 +3,8 @@ from timeit import default_timer as timer
 
 
 start = timer()
-x = timing_example.multiply(2, 4)
+for _ in range(1000):
+    x = timing_example.multiply(2, 4)
 elapsed = round(timer() - start, 5)
 
-print(x)
 print(elapsed)

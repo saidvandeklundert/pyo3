196e1ad5602881f80f1f1a9fd425e8ee329fa1ad
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-23 20:32:01 +0100

    adding

--- examples/timing/run.py
+++ examples/timing/run.py
@@ -2,9 +2,26 @@ import timing_example
 from timeit import default_timer as timer
 
 
-start = timer()
-for _ in range(1000):
-    x = timing_example.multiply(2, 4)
-elapsed = round(timer() - start, 5)
+def multiply(a: int, b: int) -> int:
+    """
+    This function is the same as timing_example.multiply, which is written as follows:
 
-print(elapsed)
+    fn multiply(a: isize, b: isize) -> PyResult<isize> {
+        Ok(a * b)
+    }
+
+    """
+    return a * b
+
+
+start = timer()
+for _ in range(100000):
+    x = timing_example.multiply(2, 4)
+calling_rust_function = round(timer() - start, 5)
+
+start = timer()
+for _ in range(100000):
+    x = multiply(2, 4)
+calling_python_function = round(timer() - start, 5)
+
+print(calling_rust_function, calling_python_function)

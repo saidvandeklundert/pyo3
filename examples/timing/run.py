585118226e933f0cc23680ab1651fcd0d54dabcc
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-23 20:35:03 +0100

    adding

--- examples/timing/run.py
+++ examples/timing/run.py
@@ -14,14 +14,22 @@ def multiply(a: int, b: int) -> int:
     return a * b
 
 
+function_calls = 10000000
 start = timer()
-for _ in range(10000000):
+for _ in range(function_calls):
     x = timing_example.multiply(2, 4)
 calling_rust_function = round(timer() - start, 5)
 
 start = timer()
-for _ in range(10000000):
+for _ in range(function_calls):
     x = multiply(2, 4)
 calling_python_function = round(timer() - start, 5)
 
+
 print(calling_rust_function, calling_python_function)
+f"""Made {function_calls} to multiply in Rust as well as in Python.
+
+Time spend calling the Rust function: {calling_rust_function}
+
+Time spend calling the Python function: {calling_python_function}
+"""
